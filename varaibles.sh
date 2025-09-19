baad5d7ffed512376b3b024d909b488e3508eb01
Anil Kumar <anilkathoju@gmail.com>
2025-09-19 18:01:58 +0530

    some

---
+++ varaibles.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+Person1=Anil
+Person2=Sunil
+
+echo "$Person2 : Hi $Person1 how are you"
+echo "$Person1 : Hi $Person2 im good, how are you"
+echo "$Person2 : im good ,thanks $Person1"
+
+
